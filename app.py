14027efab93e55229bef645ababcf7b66928a5fe
italoScript <italoyoupw@gmail.com>
2022-09-15 01:40:26 -0300

    wtf

---
+++ app.py
@@ -0,0 +1,160 @@
+from flask import Flask, request
+from flask import send_file
+from image_transformation import ImageTransformation
+from flask_cors import CORS, cross_origin
+import numpy
+import cv2
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+# transformations = [
+#     {
+#         title: "Negativo",
+#         route: "/negativo",
+#     },
+#     {
+#         title: "Logaritmica",
+#     },
+#     {
+#         title: "Potência"
+#     },
+#     {
+#         title: "Fatiamento por plano de bits"
+#     },
+#     {
+#         title: "Equalização de histograma (local)"
+#     },
+#     {
+#         title: "Equalização de histograma (global)"
+#     },
+#     {
+#         title: "Filtro de média"
+#     },
+#     {
+#         title: "Filtro de mediana"
+#     },
+#     {
+#         title: "Filtro de Sobel"
+#     },
+#     {
+#         title: "Filtro de Laplace"
+#     },
+#     {
+#         title: "Filtro High Boost"
+#     },
+#     {
+#         title: "Filtro Roberts"
+#     },
+# ]
+@app.route("/negativo", methods=['POST'])
+def imageNegativeTransformation():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.imageNegativeTransformation()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/logaritmica", methods=['POST'])
+def imageLogTransformation():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.logarithmicTransformation()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/potencia", methods=['POST'])
+def imagePowerLawTransformation():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.powerLawTransformation(0.5)
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+# @app.route("/XXXXXXXXXXXXXXXXXXXXXXXXX", methods=['POST'])
+# def XXXXXXXXXXXXXXXXXXXXXXXXXXX():
+#     file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+#     file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+#     x = ImageTransformation(file)
+#     x.XXXXXXXXXXXXXXXXXXXXXXXXXXXXX(0.5)
+#     return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/piecewise", methods=['POST'])
+def pieceWiseTransformation():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.piecewiseLinearTransformation(50, 100, 150, 200)
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/histograma-local", methods=['POST'])
+def histogramaLocal():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.localHistogramEqualization()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/histograma-global", methods=['POST'])
+def histogramaGlobal():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.globalHistogramEqualization()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/mediana-espacial", methods=['POST'])
+def medianaEspacial():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.espacialMedianFilter( 9 )
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/media-espacial", methods=['POST'])
+def mediaEspacial():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.espacialMeanFilter( 9 )
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/laplacian", methods=['POST'])
+def laplacian():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.laplacianFilter()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/high-boost", methods=['POST'])
+def highBoost():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.highBoostFilter(0.5)
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/roberts", methods=['POST'])
+def robert():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.RobertsFilter()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+@app.route("/sobel", methods=['POST'])
+def sobel():
+    file_bytes = numpy.fromfile(request.files['image'], numpy.uint8)
+    file = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    x = ImageTransformation(file)
+    x.sobelFilter()
+    return send_file('./assets/cat-edited.png', mimetype='image/png')
+
+    
+
+
+
+
---
+++ image_transformation.py
@@ -0,0 +1,112 @@
+    
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+class ImageTransformation:
+   
+    _image:any
+    #private variable
+    
+    
+    def __init__(self, image):
+        self._image = image
+    
+    def logarithmicTransformation(self):
+        # Apply log transformation method
+        c = 255 / np.log(1 + np.max(self._image))
+        log_image = c * (np.log(self._image + 1))
+        
+        # Specify the data type so that
+        # float value will be converted to int
+        log_image = np.array(log_image, dtype = np.uint8)
+        
+        cv2.imwrite('./assets/cat-edited.png', log_image)
+    
+    def powerLawTransformation(self, gamma):
+        # Apply power law transformation method
+        c = 255 / (255 ** gamma)
+        power_image = c * (self._image ** gamma)
+        
+        # Specify the data type so that
+        # float value will be converted to int
+        power_image = np.array(power_image, dtype = np.uint8)
+        
+        cv2.imwrite('./assets/cat-edited.png', power_image)
+    
+    def piecewiseLinearTransformation(self, r1, s1, r2, s2):
+        # Apply piecewise linear transformation method
+        c = 255 / (r2 - r1)
+        piece_image = c * (self._image - r1) + s1
+        
+        piece_image = np.array(piece_image, dtype = np.uint8)
+        
+        cv2.imwrite('./assets/cat-edited.png', piece_image)
+    
+    def imageNegativeTransformation(self):
+        # Apply image negative transformation method
+        negative_image = 255 - self._image
+        #save image
+        cv2.imwrite('./assets/cat-edited.png', negative_image)
+        
+    def localHistogramEqualization(self):
+        # Apply local histogram equalization method
+        gray_image = cv2.cvtColor(self._image, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+        local_image = clahe.apply(gray_image)
+        
+        cv2.imwrite('./assets/cat-edited.png', local_image)
+        
+    def globalHistogramEqualization(self):
+        # Apply global histogram equalization method
+        gray_image = cv2.cvtColor(self._image, cv2.COLOR_BGR2GRAY)
+        global_image = cv2.equalizeHist(gray_image)
+        
+        cv2.imwrite('./assets/cat-edited.png', global_image)
+    
+    def espacialMedianFilter(self, size):
+        # Apply spacial median filter method
+        image = cv2.cvtColor(self._image, cv2.COLOR_BGR2HSV)
+        median_image = cv2.medianBlur(image, size)
+        
+        cv2.imwrite('./assets/cat-edited.png', median_image)
+    
+    def espacialMeanFilter(self, size):
+        # Apply spacial mean filter method
+        image = cv2.cvtColor(self._image, cv2.COLOR_BGR2HSV)
+        mean_image = cv2.blur(image, (size, size))
+        
+        cv2.imwrite('./assets/cat-edited.png', mean_image)
+    
+    def laplacianFilter(self):
+        # Apply laplacian filter method
+        laplacian_image = cv2.Laplacian(self._image, cv2.CV_64F)
+        
+        cv2.imwrite('./assets/cat-edited.png', laplacian_image)
+    
+    def sobelFilter(self):
+        # Apply sobel filter method
+        sobel_image = cv2.Sobel(self._image, cv2.CV_64F, 1, 0, ksize=5)
+        
+        cv2.imwrite('./assets/cat-edited.png', sobel_image)
+    
+    def highBoostFilter(self,boost_factor):
+        # Apply high boost filter method
+        image = cv2.cvtColor(self._image, cv2.COLOR_BGR2GRAY)
+        resultant_image = image.copy()
+        for i in range(1,image.shape[0]-1):
+            for j in range(1,image.shape[1]-1):
+                blur_factor = (image[i-1, j-1] + image[i-1, j] - image[i-1, j+1] + image[i, j-1] + image[i, j] + image[i, j+1] + image[i+1, j+1] + image[i+1, j] + image[i+1, j+1])/9
+                mask = boost_factor*image[i, j] - blur_factor
+                resultant_image[i, j] = image[i, j] + mask
+        
+        cv2.imwrite('./assets/cat-edited.png', resultant_image)
+    
+    def RobertsFilter(self):
+        # Apply Roberts filter method
+        roberts_image = cv2.filter2D(self._image, -1, np.array([[0, 1], [-1, 0]]))
+        
+        cv2.imwrite('./assets/cat-edited.png', roberts_image)
+        
+    
+    
